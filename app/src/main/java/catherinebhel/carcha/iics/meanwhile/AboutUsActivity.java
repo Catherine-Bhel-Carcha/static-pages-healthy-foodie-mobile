dd6dbb3c8024f4e4b71ab24397cdc024c138bd73
Catherine-Bhel-Carcha <2015081521@ust-ics.mygbiz.com>
2019-02-17 21:33:59 +0800

    Initial commit

---
+++ app/src/main/java/catherinebhel/carcha/iics/meanwhile/AboutUsActivity.java
@@ -0,0 +1,13 @@
+package catherinebhel.carcha.iics.meanwhile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
---
+++ app/src/main/java/catherinebhel/carcha/iics/meanwhile/PeopleBehindActivity.java
@@ -0,0 +1,13 @@
+package catherinebhel.carcha.iics.meanwhile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PeopleBehindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people_behind);
+    }
+}
